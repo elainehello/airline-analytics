e67e7676c34243eaccfef254e9b8fc7b4335c299
elainehello <195331169+elainehello@users.noreply.github.com>
2026-04-22 20:49:00 +0200

    Initial commit

---
+++ src/test/java/com/demo/airline/AirlineAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
